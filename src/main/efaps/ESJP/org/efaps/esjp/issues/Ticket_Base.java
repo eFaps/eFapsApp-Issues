2c00313a9a74a21f65fecf8ecfed94618d7b9356
jan@moxter.net <jan@moxter.net@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2012-07-20 02:02:19 +0000

    - adding profile to connect issues to projects via version.xml in issues
    git-svn-id: https://efaps.googlecode.com/svn/eFapsApps/issues/trunk@7805 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

--- src/main/efaps/ESJP/org/efaps/esjp/issues/Ticket_Base.java
+++ src/main/efaps/ESJP/org/efaps/esjp/issues/Ticket_Base.java
@@ -18,7 +18,6 @@
  * Last Changed By: $Author$
  */
 
-
 package org.efaps.esjp.issues;
 
 import java.util.ArrayList;
@@ -26,19 +25,27 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.UUID;
 
+import org.efaps.admin.common.NumberGenerator;
+import org.efaps.admin.common.SystemConfiguration;
 import org.efaps.admin.datamodel.Type;
 import org.efaps.admin.event.Parameter;
-import org.efaps.admin.event.Return;
 import org.efaps.admin.event.Parameter.ParameterValues;
+import org.efaps.admin.event.Return;
 import org.efaps.admin.event.Return.ReturnValues;
 import org.efaps.admin.program.esjp.EFapsRevision;
 import org.efaps.admin.program.esjp.EFapsUUID;
+import org.efaps.db.Insert;
+import org.efaps.db.Instance;
 import org.efaps.db.MultiPrintQuery;
+import org.efaps.db.PrintQuery;
 import org.efaps.db.QueryBuilder;
+import org.efaps.esjp.ci.CIFormIssues;
 import org.efaps.esjp.ci.CIIssues;
+import org.efaps.esjp.common.uiform.Create;
+import org.efaps.ui.wicket.util.EFapsKey;
 import org.efaps.util.EFapsException;
-
 
 /**
  * TODO comment!
@@ -50,8 +57,96 @@ import org.efaps.util.EFapsException;
 @EFapsRevision("$Rev$")
 public class Ticket_Base
 {
+
+    /**
+     * Method to create a ticket.
+     *
+     * @param _parameter Parameter as passed from the eFaps API.
+     * @return Return with values.
+     * @throws EFapsException on error.
+     */
+    public Return create(final Parameter _parameter)
+        throws EFapsException
+    {
+        final Create create = new Create()
+        {
+
+            @Override
+            protected void add2basicInsert(final Parameter _parameter,
+                                           final Insert _insert)
+                throws EFapsException
+            {
+                add2basicInsert4create(_parameter, _insert);
+            }
+
+            @Override
+            public void connect(final Parameter _parameter,
+                                final Instance _instance)
+                throws EFapsException
+            {
+                connect4create(_parameter, _instance);
+            }
+        };
+        return create.execute(_parameter);
+    }
+
+    /**
+     * To allow easy override.
+     *
+     * @param _parameter    Parameter as passed by the eFaps API
+     * @param _insert       Insert the values can be added to
+     * @throws EFapsException on error
+     */
+    protected void connect4create(final Parameter _parameter,
+                                  final Instance _instance)
+        throws EFapsException
+    {
+        final Instance projectInst = Instance.get(_parameter
+                        .getParameterValue(CIFormIssues.Issues_TicketForm.project.name));
+        if (projectInst.isValid()) {
+            // CIProjects.Issues_Ticket2ProjectService
+            final Insert insert = new Insert(UUID.fromString("a685e79a-c320-4dcb-ad3f-6bbbfecc5850"));
+            insert.add("FromLink", _instance.getId());
+            insert.add("ToLink", projectInst.getId());
+            insert.execute();
+        }
+    }
+
+    /**
+     * Add additional values to the basic insert, prior to execution.
+     * To allow easy override.
+     * @param _parameter    Parameter as passed by the eFaps API
+     * @param _insert       Insert the values can be added to
+     * @throws EFapsException on error
+     */
+    protected void add2basicInsert4create(final Parameter _parameter,
+                                          final Insert _insert)
+        throws EFapsException
+    {
+        final Instance projectInst = Instance.get(_parameter
+                        .getParameterValue(CIFormIssues.Issues_TicketForm.project.name));
+
+        if (projectInst.isValid()) {
+            final PrintQuery print = new PrintQuery(projectInst);
+            print.addAttribute("Contact");
+            print.execute();
+
+            _insert.add(CIIssues.Ticket.ContactLink, print.getAttribute("Contact"));
+
+            // Issues_Configuration
+            if (SystemConfiguration.get(UUID.fromString("1aadb5f5-c762-46ce-8bb0-071605f382e1"))
+                            .getAttributeValueAsBoolean("Ticket_AutomaticNumbering")) {
+                // Issues_TicketSequence
+                final NumberGenerator numGen = NumberGenerator.get(
+                                UUID.fromString("5ea2e67b-7155-499c-b160-ae4bc2fc2f7e"));
+                _insert.add(CIIssues.Ticket.Name, numGen.getNextVal());
+            }
+        }
+    }
+
     /**
      * Method for auto-complete Tickets.
+     *
      * @param _parameter Parameter as passed from the eFaps API.
      * @return Return with values.
      * @throws EFapsException on error.
@@ -63,7 +158,7 @@ public class Ticket_Base
         final List<Map<String, String>> list = new ArrayList<Map<String, String>>();
         final Map<?, ?> properties = (Map<?, ?>) _parameter.get(ParameterValues.PROPERTIES);
         final String typeStr = properties.containsKey("Type") ? (String) properties.get("Type")
-                                                           : CIIssues.TicketAbstract.getType().getName();
+                        : CIIssues.TicketAbstract.getType().getName();
         final String keyStr = properties.containsKey("Key") ? (String) properties.get("Key") : "OID";
         final Map<String, Map<String, String>> tmpMap = new TreeMap<String, Map<String, String>>();
         if (input.length() > 0) {
@@ -73,19 +168,19 @@ public class Ticket_Base
                 queryBldr.addWhereAttrMatchValue(CIIssues.TicketAbstract.Name, input + "*").setIgnoreCase(true);
                 final MultiPrintQuery multi = queryBldr.getPrint();
                 multi.addAttribute(CIIssues.TicketAbstract.Name,
-                                   CIIssues.TicketAbstract.Description);
+                                CIIssues.TicketAbstract.Description);
                 multi.addAttribute(keyStr);
                 multi.execute();
                 while (multi.next()) {
                     final String name = multi.<String>getAttribute(CIIssues.TicketAbstract.Name);
                     final String descr = multi.<String>getAttribute(CIIssues.TicketAbstract.Description);
                     final String choice = name + " - " + descr.substring(0, descr.length() > 20 ? 20 : descr.length())
-                        + (descr.length() > 20 ? "..." : "");
+                                    + (descr.length() > 20 ? "..." : "");
                     final Object key = multi.getAttribute(keyStr);
                     final Map<String, String> map = new HashMap<String, String>();
-                    map.put("eFapsAutoCompleteKEY", key.toString());
-                    map.put("eFapsAutoCompleteVALUE", name);
-                    map.put("eFapsAutoCompleteCHOICE", choice);
+                    map.put(EFapsKey.AUTOCOMPLETE_KEY.getKey(), key.toString());
+                    map.put(EFapsKey.AUTOCOMPLETE_VALUE.getKey(), name);
+                    map.put(EFapsKey.AUTOCOMPLETE_CHOICE.getKey(), choice);
                     tmpMap.put(choice, map);
                 }
             }
