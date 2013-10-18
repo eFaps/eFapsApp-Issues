0f73e1ae8494c5ad7d4044e81fa810a65c284a11
jose.mallqui@efaps.org <jose.mallqui@efaps.org@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2013-10-18 19:42:47 +0000

    - issues: create and update confoguration files in package util
    git-svn-id: https://efaps.googlecode.com/svn/eFapsApps/issues/trunk@10582 fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0

---
+++ src/main/efaps/ESJP/org/efaps/esjp/util/Issues.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2003 - 2013 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Revision:        $Rev$
+ * Last Changed:    $Date$
+ * Last Changed By: $Author$
+ */
+
+package org.efaps.esjp.util;
+
+import java.util.UUID;
+
+import org.efaps.admin.common.SystemConfiguration;
+import org.efaps.admin.program.esjp.EFapsRevision;
+import org.efaps.admin.program.esjp.EFapsUUID;
+import org.efaps.util.cache.CacheReloadException;
+
+/**
+ * TODO comment!
+ *
+ * @author The eFaps Team
+ * @version $Id$
+ */
+@EFapsUUID("1a3f8344-e4f7-4cf2-aeb5-34bcbc202357")
+@EFapsRevision("$Rev$")
+public final class Issues
+{
+    /**
+     * Singelton.
+     */
+    private Issues()
+    {
+    }
+
+    /**
+     * @return the SystemConfigruation for Issues
+     * @throws CacheReloadException on error
+     */
+    public static SystemConfiguration getSysConfig()
+        throws CacheReloadException
+    {
+        // Issues-Configuration
+        return SystemConfiguration.get(UUID.fromString("1aadb5f5-c762-46ce-8bb0-071605f382e1"));
+    }
+}
---
+++ src/main/efaps/ESJP/org/efaps/esjp/util/IssuesSettings.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2003 - 2013 The eFaps Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Revision:        $Rev$
+ * Last Changed:    $Date$
+ * Last Changed By: $Author$
+ */
+
+
+package org.efaps.esjp.util;
+
+import org.efaps.admin.program.esjp.EFapsRevision;
+import org.efaps.admin.program.esjp.EFapsUUID;
+
+
+/**
+ * TODO comment!
+ *
+ * @author The eFaps Team
+ * @version $Id$
+ */
+@EFapsUUID("b1a37390-2b56-4e3e-bc1c-fe5856b3f941")
+@EFapsRevision("$Rev$")
+public interface IssuesSettings
+{
+
+    /**
+     * UUID's. <br/>
+     * UUID's of the documents with the accounting amount configuration.
+     */
+    String DOCUMENT_DOCPERCONF = "org.efaps.accounting.Document2DocumentPeriodeConfig";
+
+    /**
+     * Rate currency type used for calculate debit and credit positions for a
+     * transaction (ERP_CurrencyRateClient, Accounting_ERP_CurrencyRateAccounting).
+     */
+    String RATECURTYPE4DOCS = "org.efaps.accounting.RateCurrencyType4Documents";
+
+    /**
+     * Account name for credit to round when the difference between credit and debit is minimum.
+     */
+    String ROUNDINGCREDIT = "RoundingCredit";
+
+    /**
+     * Account name for debit to round when the difference between credit and debit is minimum.
+     */
+    String ROUNDINGDEBIT = "RoundingDebit";
+
+    /**
+     * Account name for credit or debit to complete the total credit or debit amount, when the
+     * difference between credit and debit is bigger than the minimum.
+     */
+    String TRANSFERACCOUNT = "TransferAccount";
+
+}
